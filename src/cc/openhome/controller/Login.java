80a74fb6de0cca41d4ac66f6c4dc6e93acc16695
lalahaha323 <2665687186@qq.com>
2019-03-06 22:07:13 +0800

    添加登录失败的返回页面

--- src/cc/openhome/controller/Login.java
+++ src/cc/openhome/controller/Login.java
@@ -19,6 +19,8 @@ public class Login extends HttpServlet {
         if(checkLogin(username, password)) {
             request.getRequestDispatcher(SUCCESS_VIEW)
                     .forward(request, response);
+        }else {
+            response.sendRedirect(ERROR_VIEW);
         }
     }
     private boolean checkLogin(String username, String password) throws IOException {
